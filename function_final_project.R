19baba50efc3874d23784b86f67d4c211a82ed12
NamikoMachida <nm0076@mix.wvu.edu>
2019-11-10 19:39:44 -0500

    [experiments on ge utional code, done.  can't be read in for loop. Next step: How to apply a for loop with a vector and create multiple tables.

--- function_final_project.R
+++ function_final_project.R
@@ -1,3 +1,47 @@
-col.in.world <- function (Rank, Taxon) {
-data <- idig_search_records(rq=list(Rank=Taxon))
+# not recommended due to large data size.
+# How about using a loop to search in multiple order levels?
+# List of order names for specified class needed. (May be available from PBDB??)
+collection_class <- function (Taxon) {
+  data <- idig_search_records(rq=list(class=Taxon), fields = "all")
+  head(data, n=10)
+}
+
+# Order level serach
+# (Use If statement to report errors if inappropriate rank is provided (order, family, genus))
+collection_order <- function (Taxon) {
+  data <- idig_search_records(rq=list(order=Taxon), fields = "all")
+  head(data, n=10)
+}
+
+# Family level search
+collection_family <- function (Taxon) {
+  data <- idig_search_records(rq=list(family=Taxon), fields = "all")
+  head(data, n=10)
+}
+
+# Genus level search
+collection_genus <- function (Taxon) {
+  data <- idig_search_records(rq=list(genus=Taxon), fields = "all")
+  # extract neccessarycolumns.
+  col_needed <- c("institutioncode", "catalognumber", "order", "family", "genus", "specificepithet", "earliestperiodorlowestsystem", "latestperiodorhighestsystem", "country", "county", "stateprovince", "formation")
+  data_selected <- select(data, col_needed)
+  # make a list of institution codes.
+  # convert it into a vector
+  inst_code_list <- unique(data_selected$institutioncode)
+  as.vector(inst_code_list)
+  # for loop to make a table for each institution
+  # delete institution code
+  # make a table (Huge!!)
+  data_fortab <- data_selected
+  data_fortab$institutioncode <- NULL
+  kable(data_fortab, format = "html", col.names = c("ColID", "Order", "Fam", "Gen", "Sp.", "eAge", "lAge", "Country", "County", "State", "Fm."), align = "l")
+}
+
+
+
+
+# Species level serach
+collection_species <- function (Taxon) {
+  data <- idig_search_records(rq=list(species=Taxon), fields = "all")
+  head(data, n=10)
 }
