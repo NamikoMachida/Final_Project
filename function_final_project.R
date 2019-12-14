d6ca83cc69fc36771d65f9276559479bafb61ac1
Namiko Machida <nm0076@mail.wvu.edu>
2019-12-13 20:01:20 -0500

    Capitalization of geologic period enabled by adding new function cap_head_age. Function  and  are merged into single function and now named . Argument defaults are added as Path = NULL and Search_Missing_Taxa = FALSE. If statements are added to species comparison and kable sections to deal with Path = NULL. Now,  function can conduct simple specimen search, as well as species comparison if needed. Next, create error messages and README documentation.

--- function_final_project.R
+++ function_final_project.R
@@ -1,43 +1,16 @@
-# not recommended due to large data size.
-# How about using a loop to search in multiple order levels?
-# List of order names for specified class needed. (May be available from PBDB??)
-
 #capitalize function
-cap_head <- function(text) {
-  substr(text, 1, 1) <- toupper(substr(text, 1, 1))
-  text
+cap_head <- function(string) {
+  text <- strsplit(string, " ")[[1]]
+  substring(text, 1,1) <- toupper(substring(text, 1,1))
+  paste(text, sep = "", collapse = " ")
 }
 
-
-# All rank search!
-collection <- function (Rank, Taxon) {
-  # get data from idigbio
-  listname <- c(Rank)
-  idiglist <- list(Taxon)
-  names(idiglist) <- listname
-  data <- idig_search_records(rq=idiglist, fields = "all")
-  
-  # extract neccessary columns.
-  col_needed <- c("institutioncode", "catalognumber", "order", "family", "genus", "specificepithet", "earliestperiodorlowestsystem", "latestperiodorhighestsystem", "country", "stateprovince", "county", "formation")
-  data_selected <- select(data, col_needed)
-  #sort by institutional code, genus, species.
-  data_selected_sorted <- data_selected[order(data_selected$institutioncode, data_selected$genus, data_selected$specificepithet),]
-  # delete rows if institutional code is NA.
-  data_selected_sorted <- data_selected_sorted[!is.na(data_selected_sorted$institutioncode), ]
-  
-
-  # Capitalise certain columns (Order, Family, Genus, Country, County, Fm.)  
-  col_to_cap <- c("order","family","genus","country","stateprovince","county","formation")
-  data_selected_sorted[,col_to_cap] <- apply(data_selected_sorted[,col_to_cap], 2, cap_head)
-  
-  # Make a table
-  data_selected_sorted$institutioncode <- NULL
-  kable(data_selected_sorted, format = "html", col.names = c("InstCode", "Col.ID", "Order", "Family", "Genus", "species", "earliest", "latest", "Country", "State", "County", "Fm."), align = "l", row.names = FALSE) %>%
-    kable_styling(bootstrap_options = c("striped", "condensed"), full_width = F, position = "left", font_size = 12, fixed_thead = T)%>%
-    # Header arrangement
-    add_header_above(c(" " = 2, "Classification" = 4, "Age" = 2, "Locality" = 4))%>%
-    # Group rows with institutional categories.
-    collapse_rows(columns = 1, valign = "top")
+# capitalize function for age columns
+cap_head_age <- function(string) {
+  text <- strsplit(string, " ")[[1]]
+  periods <- c("siderian", "rhyacian", "orosirian", "statherian", "calymmian", "ectasian", "stenian", "tonian", "cryogenian", "ediacaran", "cambrian", "ordovician", "silurian", "devonian", "carboniferous", "permian", "triassic", "jurassic", "cretaceous", "paleogene", "neogene", "tertiary", "quaternary")
+  substring(text[text %in% periods], 1,1) <- toupper(substring(text[text %in% periods], 1,1))
+  paste(text, sep = "", collapse = " ")
 }
 
 
@@ -45,44 +18,61 @@ collection <- function (Rank, Taxon) {
 
 # General taxon search and Species list comparison
 # Path can be a path to a taxon file of .CSV or comma delimited text file.
-compare_taxa <- function (Rank, Taxon, Path, Search_Missing_Taxa) {  
+# Taxon argument accept multiple taxa as vector.
+collection_search <- function (Rank, Taxon, Path = NULL, Search_Missing_Taxa = FALSE) {  
   # get data from idigbio
-  listname <- c(Rank)
   idiglist <- list(Taxon)
-  names(idiglist) <- listname
+  names(idiglist) <- c(Rank)
   data <- idig_search_records(rq=idiglist, fields = "all")
   
   # extract neccessary columns.
   col_needed <- c("institutioncode", "catalognumber", "order", "family", "genus", "specificepithet", "earliestperiodorlowestsystem", "latestperiodorhighestsystem", "country", "stateprovince", "county", "formation")
-  data_selected <- select(data, col_needed)
+  data_selected <- data[, col_needed]
   #sort by institutional code, genus, species.
   data_selected_sorted <- data_selected[order(data_selected$institutioncode, data_selected$genus, data_selected$specificepithet),]
   # delete rows if institutional code is NA.
   data_selected_sorted <- data_selected_sorted[!is.na(data_selected_sorted$institutioncode), ]
+
   
   
   # Capitalise certain columns (Order, Family, Genus, Country, County, Fm.)
   col_to_cap <- c("order","family","genus","country","stateprovince","county","formation")
-  data_selected_sorted[,col_to_cap] <- apply(data_selected_sorted[,col_to_cap], 2, cap_head)
+  data_selected_sorted[,col_to_cap] <- apply(data_selected_sorted[,col_to_cap], c(1,2), cap_head)
+  # Capitalize age columns
+  col_to_cap_age <- c("earliestperiodorlowestsystem", "latestperiodorhighestsystem")
+  data_selected_sorted[,col_to_cap_age] <- apply(data_selected_sorted[,col_to_cap_age], c(1,2), cap_head_age)
   
-  # Read my taxon list file using function argument PATH.
-  MyTaxa <- read.delim(file = Path, header = FALSE, sep = ",", col.names = c("Genus", "species"))
-  # Conduct get row numbers of taxa from data_selected_sorted that can be found within MyTaxa.
-  position <- which(data_selected_sorted[, "genus"] %in% MyTaxa[, "Genus"] & data_selected_sorted[, "specificepithet"] %in% MyTaxa[, "species"])
-  # If Search_Missing_Taxa argument is TRUE, reverse the previous search and produce row numbers of taxa that are missing from MyTaxa.
-  if (Search_Missing_Taxa == TRUE){
-    all_rows <- 1:nrow(data_selected_sorted)
-    position <- all_rows[!all_rows %in% position]
+  
+  position <- c()
+  if (!is.null(Path)){
+    # Read my taxon list file using function argument PATH.
+    MyTaxa <- read.delim(file = Path, header = FALSE, sep = ",", col.names = c("Genus", "species"))
+    # Conduct get row numbers of taxa from data_selected_sorted that can be found within MyTaxa.
+    position <- which(data_selected_sorted[, "genus"] %in% MyTaxa[, "Genus"] & data_selected_sorted[, "specificepithet"] %in% MyTaxa[, "species"])
+      # If Search_Missing_Taxa argument is TRUE, reverse the previous search and produce row numbers of taxa that are missing from MyTaxa.
+      if (Search_Missing_Taxa == TRUE){
+        all_rows <- 1:nrow(data_selected_sorted)
+        position <- all_rows[!all_rows %in% position]
+      }
   }
-  
 
   # Make a table.
-  kable(data_selected_sorted, format = "html", col.names = c("InstCode", "Col.ID", "Order", "Family", "Genus", "species", "earliest", "latest", "Country", "State", "County", "Fm."), align = "l", row.names = FALSE) %>%
-    kable_styling(bootstrap_options = c("striped", "condensed"), full_width = F, position = "left", font_size = 12, fixed_thead = T)%>%
-    # Header arrangement
-    add_header_above(c(" " = 2, "Classification" = 4, "Age" = 2, "Locality" = 4))%>%
-    # Highlight the rows specified by position.
-    row_spec(position, color = "red")%>%
-    # Group rows with institutional categories.
-    collapse_rows(columns = 1, valign = "top")
+  if (length(position) >= 1){
+    kable(data_selected_sorted, format = "html", col.names = c("InstCode", "Col.ID", "Order", "Family", "Genus", "species", "earliest", "latest", "Country", "State", "County", "Fm."), align = "l", row.names = FALSE) %>%
+      kable_styling(bootstrap_options = c("striped", "condensed"), full_width = F, position = "left", font_size = 12, fixed_thead = T)%>%
+      # Header arrangement
+      add_header_above(c(" " = 2, "Classification" = 4, "Age" = 2, "Locality" = 4))%>%
+      # Highlight the rows specified by position.
+      row_spec(position, color = "red")%>%
+      # Group rows with institutional categories.
+      collapse_rows(columns = 1, valign = "top")
+  }else{
+    kable(data_selected_sorted, format = "html", col.names = c("InstCode", "Col.ID", "Order", "Family", "Genus", "species", "earliest", "latest", "Country", "State", "County", "Fm."), align = "l", row.names = FALSE) %>%
+      kable_styling(bootstrap_options = c("striped", "condensed"), full_width = F, position = "left", font_size = 12, fixed_thead = T)%>%
+      # Header arrangement
+      add_header_above(c(" " = 2, "Classification" = 4, "Age" = 2, "Locality" = 4))%>%
+      # Group rows with institutional categories.
+      collapse_rows(columns = 1, valign = "top")%>%
+      scroll_box(width = "100%", height = "500px")
+  }
 }
